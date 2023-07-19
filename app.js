d580939741cd08bc94bc86922b17d5de83972912
AskarKadir <askarkadir07@gmail.com>
2023-07-19 15:08:40 +0700

    use categories router

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const logger = require("morgan");
 
 const app = express();
 
+const categoriesRouter = require("./app/api/v1/categories/router");
+const v1 = "/api/v1/cms";
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -16,5 +19,7 @@ app.get("/", (req, res) => {
     message: "Hello World",
   });
 });
+
+app.use(v1, categoriesRouter);
 
 module.exports = app;
